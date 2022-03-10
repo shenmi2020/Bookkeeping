8e4fceb409129885ca2601719da189e40ce74fa9
shenmi2020 <shenmi2020@foxmail.com>
2022-03-10 13:59:36 +0800

    first

---
+++ config/app.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of webman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+return [
+    'debug' => true,
+    'default_timezone' => 'Asia/Shanghai',
+    'public_path' => base_path() . DIRECTORY_SEPARATOR . 'public',
+    'runtime_path' => base_path(false) . DIRECTORY_SEPARATOR . 'runtime',
+];
