5192d8b84adec161e4e19ce39e5e43ac77ead953
shenmi <shenmi2020@foxmail.com>
2022-03-15 21:47:38 +0800

    db

--- config/database.php
+++ config/database.php
@@ -12,4 +12,24 @@
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
-return [];
+return [
+    // 默认数据库
+    'default' => 'mysql',
+    // 各种数据库配置
+    'connections' => [
+        'mysql' => [
+            'driver'      => 'mysql',
+            'host'        => '123.57.212.101',
+            'port'        => 3306,
+            'database'    => 'book',
+            'username'    => 'tao',
+            'password'    => 'tao@123!@#',
+            'unix_socket' => '',
+            'charset'     => 'utf8',
+            'collation'   => 'utf8_unicode_ci',
+            'prefix'      => '',
+            'strict'      => true,
+            'engine'      => null,
+        ],
+    ]
+];
