475ca3fc50f44ac725608373c318ea300f7683be
shenmi2020 <shenmi2020@foxmail.com>
2022-05-07 10:42:11 +0800

    work

--- config/process.php
+++ config/process.php
@@ -15,31 +15,31 @@
 
 return [
     // File update detection and automatic reload
-    'monitor' => [
-        'handler' => process\Monitor::class,
-        'reloadable' => false,
-        'constructor' => [
-            // Monitor these directories
-            'monitor_dir' => [
-                app_path(),
-                config_path(),
-                base_path() . '/process',
-                base_path() . '/support',
-                base_path() . '/resource',
-                base_path() . '/.env',
-            ],
-            // Files with these suffixes will be monitored
-            'monitor_extensions' => [
-                'php', 'html', 'htm', 'env'
-            ]
-        ]
-    ],
-    'task'  => [
-        'handler'  => process\Task::class
-    ],
-    'websocket_test' => [
-        'handler' => process\Pusher::class,
-        'listen'  => 'websocket://0.0.0.0:8888',
-        'count'   => 1,
-    ]
+    // 'monitor' => [
+    //     'handler' => process\Monitor::class,
+    //     'reloadable' => false,
+    //     'constructor' => [
+    //         // Monitor these directories
+    //         'monitor_dir' => [
+    //             app_path(),
+    //             config_path(),
+    //             base_path() . '/process',
+    //             base_path() . '/support',
+    //             base_path() . '/resource',
+    //             base_path() . '/.env',
+    //         ],
+    //         // Files with these suffixes will be monitored
+    //         'monitor_extensions' => [
+    //             'php', 'html', 'htm', 'env'
+    //         ]
+    //     ]
+    // ],
+    // 'task'  => [
+    //     'handler'  => process\Task::class
+    // ],
+    // 'websocket_test' => [
+    //     'handler' => process\Pusher::class,
+    //     'listen'  => 'websocket://0.0.0.0:8888',
+    //     'count'   => 1,
+    // ]
 ];
